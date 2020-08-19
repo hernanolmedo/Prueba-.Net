9ef8a6104b887444cf2924c3a971d257e904587d
Hernán <hernan.olmedo@usach.cl>
2020-08-18 23:45:53 -0400

    first commit

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Prueba_.Net.Models;
+
+namespace Prueba_.Net.Controllers
+{
+    [ApiController]
+    [Route("login")]
+    public class LoginController : ControllerBase
+    {   
+        private bool login(UserLogin user)
+        {
+            return true;
+        }
+
+        [HttpPost]
+        public IActionResult  Post([FromBody] UserLogin user)
+        {
+            IActionResult response;
+            response = BadRequest();
+            if(login(user))
+            {
+                response = Ok(new { token = "Jwt" });
+            }
+            return response;
+        }
+    }
+}
